9a1fdf0aa419600a6fce567c618b5402b1f850ab
Fer Hernandez <fh.1111@gmail.com>
2018-10-28 19:45:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
 
-  if (array.length > 1)
+  if (array.length > 2)
     last = array.pop
     #puts "#{last}"
 
@@ -8,7 +8,10 @@ def oxford_comma(array)
     newstring = array.join(", ")+ " and " + last
     puts "#{newstring}"
     return newstring
+  elsif array.lenght > 1
+    return array[0] + " and " array[1]
   else
+
     puts "#{array[0]}"
     return array[0]
   end
