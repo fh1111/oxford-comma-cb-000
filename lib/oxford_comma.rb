44bbad8010fe34b432ab62568e06788a2241295f
Fer Hernandez <fh.1111@gmail.com>
2018-10-28 19:30:10 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,9 @@ def oxford_comma(array)
   puts "#{last}"
   newarray = array.pop
   puts "#{newarray}"
-  string2 = newarray.join(",")+ "and" + last
-  puts "#{string2}"
-  return string2
+  #string2 = newarray.join(",")+ "and" + last
+  #puts "#{string2}"
+  #return string2
 
 end
 
